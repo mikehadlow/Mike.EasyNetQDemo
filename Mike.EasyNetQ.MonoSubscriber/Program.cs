061de6f805e08ab186d52cc56edad00f6ebeac6c
Mike Hadlow <mike@suteki.co.uk>
2012-09-02 11:12:07 +0100

    Added Mono Projects

---
+++ Mike.EasyNetQ.MonoSubscriber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EasyNetQ;
+using Mike.EasyNetQDemo.Messages;
+
+namespace Mike.EasyNetQDemo.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var topic = args.Length > 0 ? args[0] : "#";
+
+            using (var bus = RabbitHutch.CreateBus("host=localhost", new NullLogger()))
+            {
+                bus.Subscribe<WordMessage>("word_subscriber", topic, message => Console.WriteLine(message.Word));
+
+                Console.WriteLine("Subscription Started. Hit any key quit");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Mike.EasyNetQDemo.MonoPublisher/WordLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Mike.EasyNetQDemo
+{
+    public class WordLoader
+    {
+        public static IEnumerable<string> LoadWords()
+        {
+            var assemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (assemblyLocation == null)
+            {
+                throw new ApplicationException("Could not retrieve assembly location");
+            }
+
+            var path = Path.Combine(assemblyLocation, "Words.txt");
+
+			if(!File.Exists(path))
+			{
+				Console.WriteLine("Words.txt not found at '{0}'", path);
+			}
+
+            using(var reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                    yield return line;
+            }
+        }
+    }
+}
